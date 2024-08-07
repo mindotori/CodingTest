d999c7488010123d063738f6191272faf12c707d
mindotori <148763440+mindotori@users.noreply.github.com>
2024-08-07 22:31:10 +0900

    Programmers: 문자열을 정수로 바꾸기

---
+++ src/Lv1/FromStringToInt.java
@@ -0,0 +1,11 @@
+package Lv1;
+
+public class FromStringToInt {
+    static class Solution {
+        public int solution(String s) {
+            int answer = Integer.parseInt(s);
+
+            return answer;
+        }
+    }
+}
