158913923ecb68bc9d9b618989f17bda51cef950
mindotori <148763440+mindotori@users.noreply.github.com>
2024-08-12 04:03:06 +0900

    Programmers: 문자열 다루기 기본

---
+++ src/Lv1/TreatString.java
@@ -0,0 +1,18 @@
+package Lv1;
+
+public class TreatString {
+        public boolean solution(String s) {
+            boolean answer = true;
+            if(s.length()!=4&&s.length()!=6){
+                return false;
+            }
+            for(int i=0;i<s.length();i++){
+                if(s.charAt(i)<'0'||s.charAt(i)>'9'){
+                    return false;
+                }
+            }
+            answer = true;
+            return answer;
+        }
+
+}
