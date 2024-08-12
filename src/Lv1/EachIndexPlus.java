02344f9486c2fd806a89178e17d2458ab4bf92a9
mindotori <148763440+mindotori@users.noreply.github.com>
2024-08-12 21:52:29 +0900

    Programmers: 자릿수 더하기 주석 수정

--- src/Lv1/EachIndexPlus.java
+++ src/Lv1/EachIndexPlus.java
@@ -5,6 +5,7 @@ public class EachIndexPlus {
         int answer = 0;
         String nn = Integer.toString(n);
         for(int i=0;i<nn.length();i++){
+            // Character.getNumericValue(char) = char을 int로 형변환
             answer += Character.getNumericValue(nn.charAt(i));
         }
 
