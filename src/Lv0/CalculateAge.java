e3b436f465f5d19bff49d9e6a8ae69809b60a304
mindotori <148763440+mindotori@users.noreply.github.com>
2024-08-15 02:00:58 +0900

    Programmers: [PCCE 기출문제] 3번/나이계산

---
+++ src/Lv0/CalculateAge.java
@@ -0,0 +1,19 @@
+package Lv0;
+import java.util.*;
+public class CalculateAge {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();    // 태어난 연도
+        String age_type = sc.next();  // 나이 유형 ("Korea" 또는 "Year")
+        int answer = 0;
+
+        if (age_type.equals("Korea")) {
+            answer = 2030 - year + 1;  // 한국식 나이 계산
+        }
+        else if (age_type.equals("Year")) {
+            answer = 2030 - year;  // 서양식 나이 계산
+        }
+
+        System.out.println(answer);  // 계산된 나이 출력
+    }
+}
