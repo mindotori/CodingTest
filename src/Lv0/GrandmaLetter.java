30658b0fd8282cb69c3592a5d2fe1a6917dde2d6
mindotori <148763440+mindotori@users.noreply.github.com>
2024-08-19 23:37:36 +0900

    Programmers: 편지 수정

--- src/Lv0/GrandmaLetter.java
+++ src/Lv0/GrandmaLetter.java
@@ -3,8 +3,8 @@ package Lv0;
 public class GrandmaLetter {
     public int solution(String message) {
         int answer = 0;
-        for(int i=0;i<message.length();i++){
-            answer = 2 * i + 2;
+        for(int i=0;i<=message.length();i++){
+            answer = 2 * i;
         }
         return answer;
     }
