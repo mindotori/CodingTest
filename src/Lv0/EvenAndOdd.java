84767379c2c889b89fef7bb1e55349f462cbc25c
mindotori <148763440+mindotori@users.noreply.github.com>
2024-08-23 01:08:02 +0900

    Programmers: 홀수vs짝수

---
+++ src/Lv0/EvenAndOdd.java
@@ -0,0 +1,22 @@
+package Lv0;
+
+public class EvenAndOdd {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int sum1 = 0;
+        int sum2 = 0;
+        for(int i=0;i<num_list.length;i++){
+            if(i%2==0){
+                sum1 += num_list[i];
+            } else {
+                sum2 += num_list[i];
+            }
+        }
+        if(sum1>sum2){
+            answer = sum1;
+        } else {
+            answer = sum2;
+        }
+        return answer;
+    }
+}
